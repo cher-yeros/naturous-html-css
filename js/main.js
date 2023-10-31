5b9983120e44d2f2c9443fce61fdc8af0e8e375f
cher-yeros <yerosendiriba1892@gmail.com>
2023-10-31 21:53:10 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// const caption = document.querySelector(".caption");
+// const stories = document.querySelectorAll(".story");
+// // const images = document.querySelectorAll(".img");
+// const image = document.querySelector(".img");
+// // const story = document.getElementById("story");
+
+// // const s = document.getElementsByClassName("story");
+
+// // for (let i = 0; i < d.length; i++) {
+// //     s[i].onmouseenter = () => {
+// //         caption.classList.remove("not-hovered");
+// //         caption.classList.add("hovered");
+// //         image.classList.add("img-hovered")
+// //     }
+
+// // }
+
+
+// stories.forEach(story => {
+//     story.onmouseenter = () => {
+//         // captions.forEach(caption => {
+//         caption.classList.remove("not-hovered");
+//         caption.classList.add("hovered");
+//         // })
+
+//         image.classList.add("img-hovered");
+//     };
+
+//     story.onmouseleave = () => {
+//         // captions.forEach(caption => {
+//         caption.classList.remove("hovered");
+//         caption.classList.add("not-hovered");
+//         // })
+//         image.classList.remove("img-hovered");
+//     };
+// })
